563f0edf0302c3c01a57a6452fc38e8ad13dc4ea
Sebastian Reinhold <sebastian@bitbernd.de>
2010-09-30 12:41:34 +0200

    added addInput parameter checks

--- www/formClass.php
+++ www/formClass.php
@@ -21,7 +21,7 @@ class formClass {
     }
 
     public function __call($functionName, $functionArguments) {
-        if (substr($functionName, 0, 3) == 'add') {
+        if (substr($functionName, 0, 3) === 'add') {
             $type = strtolower(str_replace('add', '', $functionName)); // @todo case insensitive
             $name = $functionArguments[0];
             $parameters = $functionArguments[1];
@@ -29,17 +29,41 @@ class formClass {
         }
     }
 
-    public function addInput($type, $name, $parameters = array()) {
-        if ($this->inputTypes[$type]) {
-            $class = $this->inputTypes[$type] . "Class";
-            foreach($this->inputs as $input) {  // @todo $name as key?
-                if ($input->getName() == $name) {
-                    throw new Exception('Element name already in use: ' . $name);
-                }
+    public function addInput($type, $name, $parameters) {
+        $this->checkType($type);
+        $this->checkName($name);
+        $this->checkParameters($parameters);
+
+        $class = $this->inputTypes[$type] . "Class";
+        $this->inputs[] = new $class($type, $name, $parameters);
+    }
+
+    private function checkName($name) {
+        if (!is_string($name)) {
+            throw new Exception('Input name needs to be a string: ' . $name);
+        }
+        if (trim($name) === '') {
+            throw new Exception('Invalid input name');
+        }
+        foreach($this->inputs as $input) {  
+            if ($input->getName() === $name) {
+                throw new Exception('Input name already in use: ' . $name);
             }
-            $this->inputs[] = new $class($type, $name, $parameters);
-        } else {
+        }
+    }
+
+    private function checkType($type) {
+        if (!is_string($type)) {
+            throw new Exception('Input type needs to be a string: ' . $type);
+        }
+        if (!$this->inputTypes[$type]) {
             throw new Exception('Unknown input type: ' . $type);
+        }
+    }
+
+    private function checkParameters($parameters) {
+        if ((isset($parameters)) && (!is_array($parameters))) {
+            throw new Exception('Input parameters need to be in an array');
         }
     }
 }
