558552c707c51085679542289e2cc6cf5dcd4e43
Frank Hellenkamp <jonas@depage.net>
2010-11-10 12:30:48 +0100

    added ability to specify available creditcard-types

--- www/creditcard.php
+++ www/creditcard.php
@@ -7,20 +7,30 @@ require_once('fieldset.php');
  **/
 class creditcard extends fieldset {
     /**
-     * overwritable method to add child elements
+     * adds creditcard-inputs to fieldset
      *
-     *  @return void
+     * @todo check regular expressions based on (?): http://www.regular-expressions.info/creditcard.html
+     *
+     * @return void
      **/
     protected function addChildElements() {
         parent::addChildElements();
 
+        $cardnames = array(
+            'visa' => "Visa",
+            'americanexpress' => "American Express",
+            'mastercard' => "MasterCard",
+        );
+        $options = array();
+        foreach ($this->cardtypes as $card) {
+            if (isset($cardnames[$card])) {
+                $options[$card] = $cardnames[$card];
+            }
+        }
+
         $this->addSelect($this->name . "_card_type", array(
             'label' => "",
-            'optionList' => array(
-                'visa' => "Visa",
-                'americanexpress' => "American Express",
-                'mastercard' => "MasterCard",
-            ),
+            'optionList' => $options,
         ));
         $this->addText($this->name . "_card_number", array(
             'label' => $this->labelNumber,
@@ -30,7 +40,7 @@ class creditcard extends fieldset {
         $this->addText($this->name . "_card_numbercheck", array(
             'label' => $this->labelCheck,
             'required' => true,
-            'validator' => "/^(?:\d[ -]*?){3}$/",
+            'validator' => "/^\d{3,4}$/",
         ));
         $this->addText($this->name . "_card_expirydate", array(
             'label' => $this->labelExpirationDate,
@@ -41,6 +51,18 @@ class creditcard extends fieldset {
             'label' => $this->labelOwner,
             'required' => true,
         ));
+    }
+
+    /**
+     * Validate the creditcard data
+     *
+     * @todo validate based on (?): http://www-sst.informatik.tu-cottbus.de/~db/doc/Java/GalileoComputing-JavaInsel/java-04.htm#t321
+     * @todo validate specific to cardtype
+     *
+     * @return void
+     **/
+    public function validate() {
+        parent::validate();
     }
 
     /**
@@ -57,7 +79,10 @@ class creditcard extends fieldset {
         $this->defaults['labelCheck'] = "CVV/CVC";
         $this->defaults['labelExpirationDate'] = "Expiration Date MM/YY";
         $this->defaults['labelOwner'] = "Card Owner";
+        $this->defaults['cardtypes'] = array(
+            "visa",
+            "americanexpress",
+            "mastercard",
+        );
     }
-
-
 }
