02f8c76d80971b83face74c8c3631d9ccf5cc797
Sebastian Reinhold <sebastian@bitbernd.de>
2011-01-25 14:46:26 +0100

    Readded tests and doxyfile

---
+++ tests/inputClassTest.php
@@ -0,0 +1,56 @@
+<?php
+require_once('../www/inputClass.php');
+
+class inputClassTestClass extends inputClass {
+    public function __construct($name, $parameters, $formName) {
+        parent::__construct($name, $parameters, $formName);
+    }
+}
+
+class inputClassTest extends PHPUnit_Framework_TestCase {
+    public function testInputNameNoStringException() {
+        try {
+            $input = new inputClassTestClass(true, array(), 'formNameString');
+        }
+        catch (InputNameNoStringException $expected) {
+            return;
+        }
+        $this->fail('Expected inputNameNoStringException.');
+    }
+    
+    public function testInvalidInputNameException() {
+        try {
+            $input = new inputClassTestClass(' ', array(), 'formNameString');
+        }
+        catch (invalidInputNameException $expected) {
+            return;
+        }
+        $this->fail('Expected invalidInputNameException.');
+    }
+
+    public function testInputParametersNoArrayException() {
+        try {
+            $input = new inputClassTestClass('inputNameString', 'string', 'formNameString');
+        }
+        catch (inputParametersNoArrayException $expected) {
+            return;
+        }
+        $this->fail('Expected inputParametersNoArrayException.');
+    }
+
+    public function testInputClassValid() {
+        $input = new inputClassTestClass('inputNameString', array(), 'formNameString');
+        $this->assertEquals(false, $input->isValid());
+        $input->validate();
+        $this->assertEquals(false, $input->isValid());
+        $input->setValue('testValue');
+        $input->validate();
+        $this->assertEquals(true, $input->isValid());
+    }
+
+    public function testGetName() {
+        $input = new inputClassTestClass('inputNameString', array(), 'formNameString');
+        $this->assertEquals('inputNameString', $input->getName());
+    }
+}
+?>
