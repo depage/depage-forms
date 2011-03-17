03c76319d8059d3d51deb4fac730be42c755cd11
Sebastian Reinhold <sebastian@bitbernd.de>
2011-03-17 15:46:16 +0100

    logs warning on regex error

--- validators/regExValidator.php
+++ validators/regExValidator.php
@@ -14,6 +14,12 @@ class regExValidator extends validator {
     }
 
     public function validate($value) {
-        return preg_match($this->regEx, $value) ? true : false;
+        $match = preg_match($this->regEx, $value) ? true : false;
+
+        if (preg_last_error() !== PREG_NO_ERROR) {
+            $this->log("Regular expression warning: error code " . preg_last_error());
+        }
+
+        return $match;
     }
 }
