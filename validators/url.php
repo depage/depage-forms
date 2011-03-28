cc02d58eafbbbfbe8cd971566ad4208e8aab64f2
Sebastian Reinhold <sebastian@bitbernd.de>
2011-03-28 12:23:15 +0200

    worked on validators
    - changed email & url validation method to use filter_var() function

--- validators/url.php
+++ validators/url.php
@@ -1,11 +1,8 @@
 <?php
 namespace depage\htmlform\validators;
 
-class url extends regEx {
-    public function __construct($log = null) {
-        parent::__construct($log);
-
-        $this->regEx = '/(((ht|f)tp(s?):\/\/)|(www\.[^ \[\]\(\)\n\r\t]+)|(([012]?[0-9]{1,2}\.){3}[012]?[0-9]{1,2})\/)([^ \[\]\(\),;&quot;\'&lt;&gt;\n\r\t]+)([^\. \[\]\(\),;&quot;\'&lt;&gt;\n\r\t])|(([012]?[0-9]{1,2}\.){3}[012]?[0-9]{1,2})/';
-        $this->patternAttribute = "";
+class url extends validator {
+    public function validate($url) {
+        return filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED + FILTER_FLAG_HOST_REQUIRED);
     }
 }
