9915be3eff341b49568a07196211612da69ba422
Sebastian Reinhold <sebastian@bitbernd.de>
2011-04-12 14:19:27 +0200

    worked on tel input validator
    - added tel validator test

--- validators/tel.php
+++ validators/tel.php
@@ -11,7 +11,7 @@ class tel extends regEx {
     public function __construct($log = null) {
         parent::__construct($log);
 
-        $this->regEx = '/^[0-9,+,(), ,]+(,[0-9]+)*$/';
+        $this->regEx = '/^[0-9\(\)\+\-\. ]*$/';
     }
 
     /**
