985be09ff38160328b342d089c254a68273120cf
Sebastian Reinhold <sebastian@bitbernd.de>
2011-05-31 16:20:21 +0200

    added simple example and element showcase example

---
+++ documentation/examples/simple.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Simple depage-forms example
+ *
+ * Contains text and email input elements and demonstrates form processing and
+ * validation. In this simple case none of the elements are set required, so an
+ * empty form would also be valid.
+ **/
+
+/**
+ * Load the library...
+ **/
+require_once('../../htmlform.php');
+
+/**
+ * Create the example form 'simpleForm'
+ **/
+$form = new depage\htmlform\htmlform('simpleForm');
+
+/**
+ * Add the various input elements to the form by calling the '"add" + element
+ * type' method.
+ * The first parameter is the name of the element; it's a unique
+ * identifier and therefore required. The optional second parameter is an array
+ * of element settings.
+ **/
+$form->addText('username', array('label' => 'User name'));
+$form->addEmail('email', array('label' => 'Email address'));
+
+/**
+ * The process method is essential to the functionality of the form. It serves
+ * various purposes:
+ *  - it validates submitted data if there is any
+ *  - it redirects to the success page if all the data is valid
+ *  - it stores the data in the session and redirects to the form to circumvent
+ *    the form resubmission problem
+ **/
+$form->process();
+
+/**
+ * Finally, if the form is valid, dump the data (for demonstration). If it's
+ * not valid (or if it hasn't been submitted yet) display the form.
+ **/
+if ($form->validate()) {
+    /**
+     * The getValues method returns an array of the form element values.
+     **/
+    var_dump($form->getValues());
+} else {
+    echo ('<link type="text/css" rel="stylesheet" href="test.css">');
+    /**
+     * Display the form.
+     **/
+    echo ($form);
+}
+
