ac3c09309900b8c8b59a589d2d3657109735e917
Sebastian Reinhold <sebastian@bitbernd.de>
2011-06-14 13:09:57 +0200

    adapted creditcard tests

--- tests/creditcardToStringTest.php
+++ tests/creditcardToStringTest.php
@@ -28,6 +28,58 @@ class creditcardToStringTest extends PHPUnit_Framework_TestCase {
                     '</select>' .
                 '</label>' .
             '</p>' . "\n" .
+            '<p id="formName-creditcardName_card_number" class="input-text" data-errorMessage="Please enter valid data!">' .
+                '<label>' .
+                    '<span class="label">Creditcard Number</span>' .
+                    '<input name="creditcardName_card_number" type="text" pattern="^(?:\d[ -]*?){13,16}$" value="">' .
+                '</label>' .
+            '</p>' . "\n" .
+            '<p id="formName-creditcardName_card_numbercheck" class="input-text" data-errorMessage="Please enter valid data!">' .
+                '<label>' .
+                    '<span class="label">CVV/CVC</span>' .
+                    '<input name="creditcardName_card_numbercheck" type="text" pattern="^\d{3,4}$" value="">' .
+                '</label>' .
+            '</p>' . "\n" .
+            '<p id="formName-creditcardName_card_expirydate" class="input-text" data-errorMessage="Please enter valid data!">' .
+                '<label>' .
+                    '<span class="label">Expiration Date MM/YY</span>' .
+                    '<input name="creditcardName_card_expirydate" type="text" pattern="^\d{2}\/\d{2}$" value="">' .
+                '</label>' .
+            '</p>' . "\n" .
+            '<p id="formName-creditcardName_card_owner" class="input-text" data-errorMessage="Please enter valid data!">' .
+                '<label>' .
+                    '<span class="label">Card Owner</span>' .
+                    '<input name="creditcardName_card_owner" type="text" value="">' .
+                '</label>' .
+            '</p>' . "\n" .
+        '</fieldset>' . "\n";
+
+        $this->assertEquals($expected, $creditcard->__toString());
+    }
+    // }}}
+
+    // {{{ testRequired()
+    /**
+     * Tests required creditcard container
+     **/
+    public function testRequired() {
+        $form = new nameTestForm();
+
+        $creditcard = new creditcard('creditcardName', array('required' => true), $form);
+        $creditcard->addChildElements();
+
+        $expected = '<fieldset id="formName-creditcardName" name="creditcardName">' .
+            '<legend>creditcardName</legend>' .
+            '<p id="formName-creditcardName_card_type" class="input-single" data-errorMessage="Please enter valid data!">' .
+                '<label>' .
+                    '<span class="label"></span>' .
+                    '<select name="creditcardName_card_type">' .
+                        '<option value="visa">Visa</option>' .
+                        '<option value="americanexpress">American Express</option>' .
+                        '<option value="mastercard">MasterCard</option>' .
+                    '</select>' .
+                '</label>' .
+            '</p>' . "\n" .
             '<p id="formName-creditcardName_card_number" class="input-text required" data-errorMessage="Please enter valid data!">' .
                 '<label>' .
                     '<span class="label">Creditcard Number <em>*</em></span>' .
