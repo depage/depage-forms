4985e2b4724c1f1a9ecf2f117733afe4f8d5e170
Frank Hellenkamp <jonas@depage.net>
2011-06-30 12:53:04 +0200

    added clean-up code for html

--- abstracts/htmldom.php
+++ abstracts/htmldom.php
@@ -7,12 +7,15 @@ class htmldom extends \DOMDocument implements \Serializable {
     /**
      * @brief Tags that are allowed inside of html
      **/
-    protected $allowedTags = array(
+    public $allowedTags = array(
         "p",
+        "h1",
+        "h2",
         "ul",
         "ol",
         "li",
 
+        "a",
         "b",
         "strong",
         "i",
@@ -26,7 +29,7 @@ class htmldom extends \DOMDocument implements \Serializable {
      *
      * @param   (DOMDocument) htmlDOM
      *
-     * @return  (\depage\htmlform\abstracts\SerDOMDocument) htmlDOM
+     * @return  (\depage\htmlform\abstracts\htmldoc) htmlDOM
      **/
     public function __construct($version = null, $encoding = null) {
         parent::__construct($version, $encoding);
@@ -74,7 +77,7 @@ class htmldom extends \DOMDocument implements \Serializable {
         }
 
         // @todo take original content-type if available
-        $success = $tmpDOM->loadHTML("<meta http-equiv=\"content-type\" content=\"text/html; charset=$encoding\">$html");
+        $success = @$tmpDOM->loadHTML("<meta http-equiv=\"content-type\" content=\"text/html; charset=$encoding\">$html");
 
         $xpath = new \DOMXPath($tmpDOM);
         $nodelist = $xpath->query("//body/*");
@@ -106,12 +109,65 @@ class htmldom extends \DOMDocument implements \Serializable {
      * @return  (void)
      **/
     public function cleanHTML($allowedTags = null) {
+        $xpath = new \DOMXPath($this);
+
         if (is_null($allowedTags)) {
             $allowedTags = $this->allowedTags;
         }
-        // clean up
         
-        // add &nbsp; to empty paragraphs to keep them
+        // {{{ remove all nodes with tagnames that are not allowed
+        $nodelist = $xpath->query("//body//*");
+
+        for ($i = $nodelist->length - 1; $i >= 0; $i--) {
+            $node = $nodelist->item($i);
+
+            if (!in_array($node->nodeName, $allowedTags)) {
+                // move child nodes before element itself
+                while ($node->firstChild != null) {
+                    if ($node->parentNode->nodeName == "body" && $node->firstChild->nodeType  == XML_TEXT_NODE) {
+                        // put text nodes into additional p when added directly to body
+                        $paragraph = $node->parentNode->insertBefore($this->createElement("p"), $node);
+                        $paragraph->appendChild($node->firstChild);
+                    } else {
+                        $node->parentNode->insertBefore($node->firstChild, $node);
+                    }
+                }
+                
+                // delete empty node
+                $node->parentNode->removeChild($node);
+            }
+        }
+        // }}}
+        // {{{ add &nbsp; to empty paragraphs to keep them
+        $nodelist = $xpath->query("//*[. = '']");
+
+        foreach ($nodelist as $node) {
+            $node->appendChild($this->createEntityReference("nbsp"));
+        }
+        // }}}
+        // {{{ make sure li-nodes are alwas inside ul, ol or menu
+        $nodelist = $xpath->query("//li");
+        $parentNodes = array("ul", "ol", "menu");
+
+        foreach ($nodelist as $node) {
+            if (!in_array($node->parentNode->nodeName, $parentNodes)) {
+                // find previous XML-element-node
+                $previous = $node->previousSibling;
+                while (!is_null($previous) && $previous->nodeType != XML_ELEMENT_NODE) {
+                    $previous = $previous->previousSibling;
+                }
+
+                if (!is_null($previous) && in_array($previous->nodeName, $parentNodes)) {
+                    // previous element is a list -> add node to it
+                    $listNode->appendChild($node);
+                } else {
+                    // create a new ul-list and add element to it
+                    $listNode = $node->parentNode->insertBefore($this->createElement("ul"), $node);
+                    $listNode->appendChild($node);
+                }
+            }
+        }
+        // }}}
     }
     // }}}
 }
