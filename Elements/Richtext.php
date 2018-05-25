2cbe352407cf511429fb64f9cbdbd5149837b201
Frank Hellenkamp <jonas@depage.net>
2018-05-25 13:33:05 +0200

    added workaround for missing normalizer class

--- Elements/Richtext.php
+++ Elements/Richtext.php
@@ -152,7 +152,7 @@ class Richtext extends Textarea
      **/
     protected function parseHtml($html)
     {
-        if ($this->normalize) {
+        if ($this->normalize && class_exists("\\Normalizer")) {
             $html = \Normalizer::normalize($html);
         }
 
